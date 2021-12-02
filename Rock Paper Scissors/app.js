54aaebb1a4b63e6186436681da824e5d61c733f7
msiyaz <48560280+msiyaz@users.noreply.github.com>
2021-12-02 02:58:20 -0330

    Add files via upload

---
+++ Rock Paper Scissors/app.js
@@ -0,0 +1,134 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    //Start the game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+
+    //Play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll(".hands img");
+
+
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function() {
+              this.style.animation = "";
+            });
+          });
+
+        //Computer Options(Generate a number between 0-2 and match it with the images declared above)
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+                //Computer Choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                setTimeout(() => {
+                    //Here is where we call compare hands
+                    compareHands(this.textContent, computerChoice);
+                    //Update Images
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                  }, 2000);
+                  //Animation
+                  playerHand.style.animation = "shakePlayer 2s ease";
+                  computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });
+    };
+
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    };
+
+    const compareHands = (playerChoice, computerChoice) => {
+        //Update text
+        const winner = document.querySelector('.winner');
+
+        //Checking for a tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = 'It is a tie';
+            return;
+        }
+
+        //Check for rock
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Player wins';
+                pScore++;
+                updateScore();
+                return;
+            } 
+            else {
+                winner.textContent = 'Computer wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+
+        //Check for paper
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Computer wins';
+                cScore++;
+                updateScore();
+                return;
+            } 
+            else {
+                winner.textContent = 'Player wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+
+         //Check for Scissors
+         if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winner.textContent = 'Computer wins';
+                cScore++;
+                updateScore();
+                return;
+            } 
+            else {
+                winner.textContent = 'Player wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+
+
+    };
+
+
+
+    //Is call all inner functions
+    startGame();
+    playMatch();
+    // updateScore();
+};
+
+//Start the game function
+
+game();
