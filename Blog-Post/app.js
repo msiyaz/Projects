ff6b3bfaaa05b0944f5cf9c6d6a40317ac6a330e
Munya <msiyaz98@gmail.com>
2022-10-05 03:46:50 -0230

    update

--- Blog-Post/app.js
+++ Blog-Post/app.js
@@ -30,6 +30,8 @@ app.get('/',(req, res) => {
     res.render('posts/index' , { posts : posts})
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
