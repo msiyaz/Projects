97eb0181e078b7d848aae2d3c20321d0f92b43cb
Munya <msiyaz98@gmail.com>
2023-01-16 05:43:55 -0330

    Move to react

--- Blog-Post/react_frontend/src/App.js
+++ Blog-Post/react_frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   
   return (
     <div className="App">
-      <Home/>
+      <Navbar/>
+      <Home />
     </div>
   );
 }
